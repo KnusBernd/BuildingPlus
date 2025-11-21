9b651fb39f2d4f1f5da351bdbc79123c6a82872f
Daniel <daniel@gymtracker.de>
2025-11-21 19:13:51 +0100

    init

---
+++ BuildingPlusPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+
+namespace BuildingPlus
+{
+
+    [BepInPlugin("BuildingPlus", "BuildingPlus", "0.0.1")]
+    public class BuildingPlusPlugin : BaseUnityPlugin
+    {
+
+    }
+}
