a81aac06f034fee2a215dfe38da62ae6e2812c55
Daniel <daniel@gymtracker.de>
2025-11-21 19:20:20 +0100

    logging and base plugin

--- BuildingPlusPlugin.cs
+++ BuildingPlusPlugin.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace BuildingPlus
 {
@@ -11,6 +13,36 @@ namespace BuildingPlus
     [BepInPlugin("BuildingPlus", "BuildingPlus", "0.0.1")]
     public class BuildingPlusPlugin : BaseUnityPlugin
     {
+
+        internal static BuildingPlusPlugin Instance { get; private set; }
+        internal static ManualLogSource LoggerInstance => Instance.Logger;
+
+        private Harmony harmony;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            // Initialize Harmony
+            harmony = new Harmony("BuildingPlus");
+            LogInfo("Plugin loaded.");
+            
+        }
+
+        public static void LogInfo(string message)
+        {
+            LoggerInstance.LogInfo($"[BuildingPlus] {message}");
+        }
+
+        public static void LogWarning(string message)
+        {
+            LoggerInstance.LogWarning($"[BuildingPlus] {message}");
+        }
+
+        public static void LogError(string message)
+        {
+            LoggerInstance.LogError($"[BuildingPlus] {message}");
+        }
 
     }
 }
